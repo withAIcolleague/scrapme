d3766ad3ab33768905ea37bf2d599e17ed8e1b3c
withAIcolleague <endurancedy@gmail.com>
2026-02-21 20:32:31 +0900

    chore: initialize Scrapme project

---
+++ project.md
@@ -0,0 +1,14 @@
+## Project Name
+
+Scrapme
+
+## Project Description
+
+scrapme is personal scarap service. anyone want scrap book of crawled images make it easy.
+
+## Project Features
+
+- 한 페이지에 내 마음대로 이미지 붙여서 나만의 스크랩북 만들기
+- 예쁘게 다양하게 꾸미기
+
+## Project Technologies
---
+++ scrapme-web/src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Space_Grotesk, Space_Mono } from "next/font/google";
+import "./globals.css";
+
+const spaceGrotesk = Space_Grotesk({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const spaceMono = Space_Mono({
+  variable: "--font-geist-mono",
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Scrapme",
+  description: "Make your personal scrapbook from images you love.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${spaceGrotesk.variable} ${spaceMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
